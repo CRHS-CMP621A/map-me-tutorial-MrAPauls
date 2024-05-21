2136d14cecfd6d0713078ce6a4bcbda434d4d349
Andrew Pauls <ajpauls@cloud.edu.pe.ca>
2024-05-21 15:43:13 -0300

    Part 3 complete

--- MapApp/script.js
+++ MapApp/script.js
@@ -1,14 +1,82 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
 
+//GLOBAL VARIABLES
+let map;
+let mapEvent;
 
+//navigator code
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    // console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+
+    // add LEAFLET SAMPLE CODE here
+    const coords = [latitude, longitude];
+    map = L.map("map").setView(coords, 13);
+
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on("click", function (mapE) {
+      mapEvent = mapE;
+
+      form.classList.remove("hidden");
+      inputDistance.focus();
+
+      // console.log(mapEvent);
+    });
+  },
+  function () {
+    alert("Could not get position.");
+  }
+);
+
+// form event listener to check if submitted/completed
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // code for adding map marker...
+
+  const lat = mapEvent.latlng.lat;
+  const lng = mapEvent.latlng.lng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("Workout")
+    .openPopup();
+
+  if (inputType.value == "cycling") {
+    inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+    inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+  }
+  form.reset();
+
+  // form.classList.add("hidden");
+});
+
+//Event Listener Toggle form input type change.
+inputType.addEventListener("change", function () {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
